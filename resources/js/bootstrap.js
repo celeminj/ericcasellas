f6bf47285e77e0ff12b733111884194fdeaeade3
Jordi <jordi.celemin@gmail.com>
2025-08-02 17:23:20 +0200

    deployssssssssssssssssss

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -7,7 +7,7 @@
 import axios from 'axios';
 window.axios = axios;
 export default axios.create({
-  baseURL: 'http://ericcasellas.celemin.me/public/api',
+  baseURL: 'https://ericcasellas.celemin.me/api',
 })
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
